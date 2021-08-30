83011d474a54e2aa4ab334aa405e7b23e1f893ca
cnugroho11 <cnugroho211@gmail.com>
2021-08-30 12:24:11 +0700

    testing

--- if-else.py
+++ if-else.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     n = int(input())
     if n % 2 == 0:
-        if n > 2 and n < 5:
+        if 2 < n < 5:
             print('Not Weird')
         elif n > 20:
             print('Not Weird')
